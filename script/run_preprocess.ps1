c9688cde869956c089a6238a282ebc2ccaa36111
HCN1222 <92087079+HCN1222@users.noreply.github.com>
2026-03-13 17:31:24 +0800

    rename file

---
+++ script/run_preprocess.ps1
@@ -0,0 +1,4 @@
+python ./src/preprocess.py `
+    --broker_data_path "./data/brokers/1440/2021-06-30_to_2026-02-11.parquet" `
+    --stock_info_dir "./data/stocks" `
+    --output_path "./data/preprocessed_data/final_vectors.parquet"
